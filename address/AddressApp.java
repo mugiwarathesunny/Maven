9ab981336a08cc4b33d1aae05ef3170923997502
Toluwalase Cephas Akindolie <000870019@mohawkcollege.ca>
2024-11-26 04:02:51 -0500

    commit

--- address/AddressApp.java
+++ address/AddressApp.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class AddressApp {
+/**
+ * The AddressApp class serves as the entry point for the application.
+ * It demonstrates a basic Java program by printing "Hello, World!" to the console.
+ */
+public class AddressApp {
+
+    /**
+     * The main method is the starting point of the application.
+     *
+     * @param args an array of command-line arguments passed to the program
+     */
+    public static void main(String[] args) {
+
+        // Prints "Hello, World!" to the console.
+        System.out.println("Hello, World!");
+    }
 }
